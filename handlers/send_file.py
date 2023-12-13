3170d8605253a75d0bcf0fa58c2a55f0effd3eda
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2023-12-13 11:00:35 +0500

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -11,7 +11,12 @@ from handlers.helpers import str_to_b64
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"Files will be deleted in 05 minutes to avoid copyright issues./n/n Please forward and save them./n/n Bhai please inko forward karlo...kuch issue hoto mujhe msg karo @badal6667rai/n/n STREAM YOUR FILES BY FORWARDING THESE FILES TO THIS BOT @filestreamerv22_bot 🙏",
+            f"Files will be deleted in 05 minutes to avoid copyright issues
+            
+            Please forward and save them Somewhere else.
+            Bhai please inko forward karlo...kuch issue hoto mujhe msg karo @badal6667rai
+            
+            STREAM YOUR FILES BY FORWARDING THESE FILES TO THIS BOT @filestreamerv22_bot 🥰🙏",
             disable_web_page_preview=True,
             quote=True
         )
