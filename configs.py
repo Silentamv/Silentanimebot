a20ba3ddb05a9996a9b4ed0f12714bd8af88aa1e
Silentamv <159916683+Silentamv@users.noreply.github.com>
2024-03-10 12:04:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", "25435105"))
-  API_HASH = os.environ.get("API_HASH", "011126265844f2d7cc7dc1a024f6bc78")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6734734174:AAERH1lMzFq2dgAi8V-Fp0WLU18_yl4OMCI")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "fileeestorebot")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002053044713"))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "ez4short.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "ade16f386d69aa3d86d6320b5b2c906996a8d2a6")
+  API_ID = int(os.environ.get("API_ID", "27505575"))
+  API_HASH = os.environ.get("API_HASH", "03bbc5e15dbddfb03fb953435b5eb028")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7028356497:AAFtBVcZE4N5dYUGSa2B8awtSuFETLxUkuA")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "silentfilestorebot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002007510451"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "Ziplinker.net")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "760615c59a2a8723d5b2e85a8ccc42f58170fecc")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "6459102722"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://SINGER:SINGER@cluster0.1dt9hoe.mongodb.net/?retryWrites=true&w=majority")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001956959333"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Aru:xitheIzcM2MVw9Lf@aru.brkfhe9.mongodb.net/?retryWrites=true&w=majority&appName=Aru")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002084706768")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002132361598"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -28,7 +28,7 @@ class Config(object):
 🌿 ɴᴇᴠᴇʀꜰᴏʟᴅ ɴᴇᴠᴇʀ ʙᴀᴄᴋᴅᴏᴡɴ 🌿
 """
   ABOUT_DEV_TEXT = f"""
-ᴍʏ ᴅᴇᴠ: [➡](https://telegram.me/badal6667rai)
+ᴍʏ ᴅᴇᴠ: [➡](https://telegram.me/silent_amv)
  
  I am Super noob Please Support My Hard Work. 🐸
 """
